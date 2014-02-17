315b36310fe5802d6eccd5b788e3f02cb5dd6bc9
blissdev <blissdev@gmail.com>
2014-02-17 00:31:27 -0600

    Use 'onPreHandler' extension point
    Switched to using onPreHandler because the payload isn't parsed yet when
    onRequest is called. The payload can be useful in determining what
    Postgres connection to use.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var pg = require('pg.js');
 
 exports.register = function(plugin, options, next) {
 
-  plugin.ext('onRequest', function(request, extNext) {
+  plugin.ext('onPreHandler', function(request, extNext) {
     var connectionString = generateConnection(options.connectionString, request);
 
     // if a connection string is not resolved, we stop the process
